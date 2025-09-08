a99dc6faa11c22fc984db53a3a3a4e4624a91668
morgantstewart <morgan@paysigil.com>
2025-09-08 16:41:39 -0400

    About Page functionality established

--- batvat/urls.py
+++ batvat/urls.py
@@ -21,7 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-        path('about/', views.about, name='about'),
-      path('bats/', views.bat_index, name='bat-index'),
+    path('about/', views.about, name='about'),
+    path('bats/', views.bat_index, name='bat-index'),
 
 ]
--- batvat/views.py
+++ batvat/views.py
@@ -9,3 +9,13 @@ from django.http import HttpResponse
 def home(request):
     # Send a simple HTML response
     return HttpResponse('<h1>Hello from BatVat</h1>')
+
+# Define the about view function
+def about(request):
+    # Send a simple HTML response for the about page
+    return HttpResponse('<h1>About BatVat</h1><p>This is the about page for the Bat Vat application!</p>')
+
+# Define the bat index view function
+def bat_index(request):
+    # Send a simple HTML response for the bats page
+    return HttpResponse('<h1>Bats Index</h1><p>Welcome to the bats section!</p>')
