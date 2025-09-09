b749794955b5d2fd44d26d6c0b41c057c721d99d
morgantstewart <morgan@paysigil.com>
2025-09-08 21:37:08 -0400

    added a Bats page

--- batvat/views.py
+++ batvat/views.py
@@ -17,5 +17,23 @@ def about(request):
 
 # Define the bat index view function
 def bat_index(request):
-    # Render the bat index template
-    return render(request, 'bats/index.html')
+    # Render the bat index template with the bats data
+    return render(request, 'bats/index.html', {'bats': bats})
+
+# views.py
+
+class Bat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+bats = [
+    Bat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Bat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Bat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Bat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
