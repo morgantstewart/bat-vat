8c7a446b1a29fbc48c62821055b82a3089b4d7d1
morgantstewart <morgan@paysigil.com>
2025-09-10 23:55:57 -0400

    corrected indentation and duplicate class definitions in Bat model

--- batvat/admin.py
+++ batvat/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Bat
+from .models import Bat, Feeding
 
 admin.site.register(Bat)
+admin.site.register(Feeding)
--- batvat/models.py
+++ batvat/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.urls import reverse
 
 
+# A tuple of 2-tuples added above our models
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
 class Bat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -14,3 +22,17 @@ class Bat(models.Model):
     def get_absolute_url(self):
         # Use the 'reverse' function to dynamically find the URL for viewing this bat's details
         return reverse('bat-detail', kwargs={'bat_id': self.id})
+
+
+# Add new Feeding model below Bat model
+class Feeding(models.Model):
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    bat = models.ForeignKey(Bat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
